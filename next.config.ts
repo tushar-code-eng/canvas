c4f8b09a09c5c6c859aa52c87e3cc392b1a8ad5b
Tushar <tusharbajaj10@gmail.com>
2025-02-13 02:32:26 +0530

    build 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
   typescript: {
     ignoreBuildErrors: true, // Ignore TypeScript errors during build
   },
